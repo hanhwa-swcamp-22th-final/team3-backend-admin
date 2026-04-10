015ba2b674753b90157a2d8d7dad3b53e28acc1b
cyhhoo <cyhhoo@gmail.com>
2026-04-10 14:46:22 +0900

    refactor: align domain keyword category with OTHERS

--- src/main/java/com/ohgiraffers/team3backendadmin/admin/command/domain/aggregate/domainkeyword/DomainCompetencyCategory.java
+++ src/main/java/com/ohgiraffers/team3backendadmin/admin/command/domain/aggregate/domainkeyword/DomainCompetencyCategory.java
@@ -1,10 +1,10 @@
 package com.ohgiraffers.team3backendadmin.admin.command.domain.aggregate.domainkeyword;
 
 public enum DomainCompetencyCategory {
-    TECHNICAL_COMPETENCE, // 기술역량
-    LEADERSHIP,           // 리더십
-    SAFETY,               // 안전
-    INNOVATION,           // 혁신
-    COLLABORATION,        // 협업
-    OTHER                 // 기타
+    TECHNICAL_COMPETENCE,
+    LEADERSHIP,
+    SAFETY,
+    INNOVATION,
+    COLLABORATION,
+    OTHERS
 }
